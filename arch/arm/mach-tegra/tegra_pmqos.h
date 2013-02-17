b3fa51be3eadbcdda3f485c76a8d276ba8b10393
Paul Beeler <sparksco@gmail.com>
2013-02-16 18:27:18 -0700

    Revert "ARM tegra:  Fix some serious and annoying bugs"
    This reverts commit 5f4c7d79ea8863bc9faa6385d68b1c1601f8cab8.

--- arch/arm/mach-tegra/tegra_pmqos.h
+++ arch/arm/mach-tegra/tegra_pmqos.h
@@ -26,6 +26,7 @@
 
 /* in kHz */
 #define BOOST_CPU_FREQ_MIN	1300000
+#define CAP_CPU_FREQ_MAX	475000
 #define T3_CPU_MIN_FREQ         102000
 
 /* in 10 MHz
@@ -34,3 +35,4 @@
 #define T3_EDP_BOOST       30
 
 extern unsigned int tegra_pmqos_boost_freq;
+extern unsigned int tegra_pmqos_cap_freq;
