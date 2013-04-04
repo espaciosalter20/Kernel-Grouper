9d64ff93c6a52d481e5f36ed67932bf4eb529cd0
Paul Beeler <sparksco@gmail.com>
2013-04-04 15:32:34 -0600

    underclock by default Signed-off-by: Paul Beeler <sparksco@gmail.com>

--- arch/arm/mach-tegra/tegra_pmqos.h
+++ arch/arm/mach-tegra/tegra_pmqos.h
@@ -26,7 +26,7 @@
 
 /* in kHz */
 #define BOOST_CPU_FREQ_MIN	1300000
-#define T3_CPU_MIN_FREQ         102000
+#define T3_CPU_MIN_FREQ         51000
 
 /* in 10 MHz
  * this effectively makes all tablets 1600Mhz capable, but is limited by
